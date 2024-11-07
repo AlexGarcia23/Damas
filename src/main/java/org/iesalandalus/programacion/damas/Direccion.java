25ac7b52bc86cc2ae6fab8a6ba69279b267d8d2f
Alex <akexgarcia47@gmail.com>
2024-11-07 17:19:06 +0100

    Creación de enumeración Direccion

---
+++ src/main/java/org/iesalandalus/programacion/damas/Direccion.java
@@ -0,0 +1,11 @@
+package org.iesalandalus.programacion.damas;
+
+public enum Direccion {
+    NORESTE("Noreste"),
+    NOROESTE("Noroeste"),
+    SURESTE("Sureste"),
+    SUROESTE("Suroeste");
+
+
+}
+
