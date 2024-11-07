a6b32088b83ec5fdcd9e26d20ac3d707870b8c3d
Alex <akexgarcia47@gmail.com>
2024-11-07 17:43:39 +0100

    Creación de la clase Posicion con sus atributos fila y columna

--- src/main/java/org/iesalandalus/programacion/damas/Color.java
+++ src/main/java/org/iesalandalus/programacion/damas/Color.java
@@ -18,4 +18,5 @@ public enum Color {
                 "cadenaAMostrar='" + cadenaAMostrar + '\'' +
                 '}';
     }
+
 }
---
+++ src/main/java/org/iesalandalus/programacion/damas/Posicion.java
@@ -0,0 +1,10 @@
+package org.iesalandalus.programacion.damas;
+
+public class Posicion {
+
+    //Atributos
+    private int fila;
+    private char columna;
+
+
+}
