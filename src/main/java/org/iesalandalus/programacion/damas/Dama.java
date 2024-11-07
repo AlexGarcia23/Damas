6e34cedd50a15cdb94a6b2d9b9855aedb2966efa
Alex <akexgarcia47@gmail.com>
2024-11-07 18:37:07 +0100

    Creación de la clase Dama con sus atributos

---
+++ src/main/java/org/iesalandalus/programacion/damas/Dama.java
@@ -0,0 +1,9 @@
+package org.iesalandalus.programacion.damas;
+
+public class Dama {
+    //Atributos
+    private Color Color;
+    private Posicion Posicion;
+    private boolean esDamaEspecial;
+
+}
