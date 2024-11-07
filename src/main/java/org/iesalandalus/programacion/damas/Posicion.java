58e6e2d1de0e3a83139f4c801c887bf2a527d80a
Alex <akexgarcia47@gmail.com>
2024-11-07 18:18:17 +0100

    Actualizacion de la clase Posicion con sus getter and setters y sus limitacionesd de rango del 1 - 8 para las filas y de la a - h para las columnas

--- src/main/java/org/iesalandalus/programacion/damas/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/damas/Posicion.java
@@ -6,5 +6,26 @@ public class Posicion {
     private int fila;
     private char columna;
 
+    //Metodos
+    public int getFila() {
+        return fila;
+    }
 
+    public void setFila(int fila) {
+        if (fila < 1 || fila > 8){
+            throw new IllegalArgumentException("El valor de la fila tiene que estar en el rango de valores 1 - 8");
+        }
+        this.fila = fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        if (columna < 'a' || columna > 'h'){
+            throw new IllegalArgumentException("El valor de la columna tiene que estar en el rango de valores de a - h ");
+        }
+        this.columna = columna;
+    }
 }
